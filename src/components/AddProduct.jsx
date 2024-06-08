7133327f494ea7d5fc5706a6356611d97c9d8678
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-08 09:27:31 +0530

    all done

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddProduct = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">ID</label>
+                            <input type="text" className="input form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">Title</label>
+                            <input type="text" className="input form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">Price</label>
+                            <input type="text" className="input form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="label form-label">Description</label>
+                            <textarea name="" id="" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">Category</label>
+                            <select name="
+                            " id="" className="form-control">
+                                <option value="mens clothing">mens clothing</option>
+                                <option value="ladies wear">ladies wear</option>
+                                <option value="bags">bags</option>
+                                <option value="watches">watches</option>
+                                <option value="bracelets">bracelets</option>
+                            </select>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">Image</label>
+                            <input type="file" name="" id="" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">Rating</label>
+                            <input type="text" className="input form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="label form-label">Count</label>
+                            <input type="text" className="input form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddProduct
